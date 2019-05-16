42fc44ca5720aad3f608736b5484e3b30323193a
wuyz <wuyongzheng@doujishipin.com>
2019-05-16 13:06:16 +0800

    代码修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
+import index2 from '@/components/index2'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/index2',
+      name: 'index2',
+      component: index2
     }
   ]
 })
